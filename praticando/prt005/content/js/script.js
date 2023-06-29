52cc2637c2465646514647ad4406721fe49c5ee6
Odair Panizzi Júnior <101676143+OdairPanizziJunior@users.noreply.github.com>
2023-06-29 10:48:14 +0200

    Initial commit

---
+++ praticando/prt005/content/js/script.js
@@ -0,0 +1,36 @@
+
+// FACEBOOK
+function trocarFace(face) {
+    face.src = "../images/footer/icons8-facebook-24-cl.png";
+  }
+  
+  function restaurarFace(face) {
+    face.src = "../images/footer/icons8-facebook-24-bk.png";
+  }
+
+//INSTAGRAM
+function trocarInsta(insta){
+    insta.src = "../images/footer/icons8-instagram-24-cl.png";
+}
+
+function restaurarInsta(insta){
+    insta.src = "../images/footer/icons8-instagram-24-bk.png";
+}
+
+//LINKEDIN
+function trocarLkdin(lkdin){
+    lkdin.src = "../images/footer/icons8-linkedin-24-cl.png";
+}
+
+function restaurarLkdin(lkdin){
+    lkdin.src = "../images/footer/icons8-linkedin-24-bk.png";
+}
+
+//YOUTUBE
+function trocarYoutb(youtb){
+    youtb.src = "../images/footer/icons8-youtube-24-cl.png";
+}
+
+function restaurarYoutb(youtb){
+    youtb.src = "../images/footer/icons8-youtube-24-bk.png";
+}
